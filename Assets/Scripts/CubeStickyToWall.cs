ef7310441e6b01f73816d74a803811d1baa4fb06
Weiwei <williamzhangww@gmail.com>
2025-07-21 21:31:06 +0300

    Update: PT100 and DT50 with cable

--- Assets/Scripts/CubeStickyToWall.cs
+++ Assets/Scripts/CubeStickyToWall.cs
@@ -6,7 +6,7 @@ public class CubeStickyToWall : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Body4")
+        if (collision.gameObject.name == "Board")
         {
             transform.SetParent(collision.transform.root); // 设置为 Wall v1 的子物体
             isOnWall = true;
@@ -15,7 +15,7 @@ public class CubeStickyToWall : MonoBehaviour
 
     void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.name == "Body4" && isOnWall)
+        if (collision.gameObject.name == "Board" && isOnWall)
         {
             transform.SetParent(null); // 脱离
             isOnWall = false;
